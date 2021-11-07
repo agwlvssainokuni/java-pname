eadcf48455ee3cd4d46d8401ad29fcaa782be6d4
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2021-11-07 20:02:12 +0900

    物理名生成　依存ライブラリ最新化。

--- pname-web/src/main/javascript/indexreact.js
+++ pname-web/src/main/javascript/indexreact.js
@@ -1,9 +1,3 @@
-import _regeneratorRuntime from "babel-runtime/regenerator";
-
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
 /*
  * Copyright 2021 agwlvssainokuni
  *
@@ -20,74 +14,35 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
  * limitations under the License.
  */
 // ENTRY
-
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { ln2pn } from "./pname-api";
 
 function PnamePanel(props) {
-	var _this = this;
+  let [data, setData] = useState("");
 
-	var _useState = useState(""),
-	    _useState2 = _slicedToArray(_useState, 2),
-	    data = _useState2[0],
-	    setData = _useState2[1];
+  const handleClick = async event => {
+    let pn = await ln2pn(event.target.value, data);
+    setData(pn);
+  };
 
-	var handleClick = function () {
-		var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee(event) {
-			var pn;
-			return _regeneratorRuntime.wrap(function _callee$(_context) {
-				while (1) {
-					switch (_context.prev = _context.next) {
-						case 0:
-							_context.next = 2;
-							return ln2pn(event.target.value, data);
-
-						case 2:
-							pn = _context.sent;
-
-							setData(pn);
-
-						case 4:
-						case "end":
-							return _context.stop();
-					}
-				}
-			}, _callee, _this);
-		}));
-
-		return function handleClick(_x) {
-			return _ref.apply(this, arguments);
-		};
-	}();
-
-	return React.createElement(
-		React.Fragment,
-		null,
-		React.createElement(
-			"div",
-			{ className: "form-group" },
-			React.createElement("textarea", { className: "form-control", rows: "20", cols: "40",
-				value: data, onChange: function onChange(event) {
-					return setData(event.target.value);
-				} })
-		),
-		React.createElement(
-			"div",
-			{ className: "form-group" },
-			[["UPPER_SNAKE", "UPPER_SNAKE"], ["LOWER_SNAKE", "lower_snake"], ["UPPER_CAMEL", "UpperCamel"], ["LOWER_CAMEL", "lowerCamel"], ["UPPER_KEBAB", "UPPER-KEBAB"], ["LOWER_KEBAB", "lower-kebab"]].map(function (e) {
-				return React.createElement(
-					"button",
-					{ className: "btn btn-primary", value: e[0], onClick: handleClick },
-					e[1]
-				);
-			}).flatMap(function (btn, i) {
-				return i === 0 ? [btn] : [" ", btn];
-			})
-		)
-	);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
+    className: "form-group"
+  }, /*#__PURE__*/React.createElement("textarea", {
+    className: "form-control",
+    rows: "20",
+    cols: "40",
+    value: data,
+    onChange: event => setData(event.target.value)
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "form-group"
+  }, [["UPPER_SNAKE", "UPPER_SNAKE"], ["LOWER_SNAKE", "lower_snake"], ["UPPER_CAMEL", "UpperCamel"], ["LOWER_CAMEL", "lowerCamel"], ["UPPER_KEBAB", "UPPER-KEBAB"], ["LOWER_KEBAB", "lower-kebab"]].map(e => /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-primary",
+    value: e[0],
+    onClick: handleClick
+  }, e[1])).flatMap((btn, i) => i === 0 ? [btn] : [" ", btn])));
 }
 
-window.onload = function () {
-	ReactDOM.render(React.createElement(PnamePanel, null), document.querySelector("#pname-web"));
+window.onload = () => {
+  ReactDOM.render( /*#__PURE__*/React.createElement(PnamePanel, null), document.querySelector("#pname-web"));
 };
